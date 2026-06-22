4c250764d162a55ae811a38709387cdff02a3661
Chris Bunny <christopherjbunny@gmail.com>
2026-06-22 11:10:17 -0500

    Completed a test for searching without any filters, tested both products with feature and non-featured.

--- src/test/java/org/yearup/service/ProductServiceTest.java
+++ src/test/java/org/yearup/service/ProductServiceTest.java
@@ -1,14 +1,37 @@
 package org.yearup.service;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.webmvc.test.autoconfigure.WebMvcTest;
-import org.springframework.test.web.servlet.MockMvc;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.yearup.models.Product;
+import org.yearup.repository.ProductRepository;
 
-@WebMvcTest(ProductService.class)
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
 public class ProductServiceTest {
 
-    @Autowired
-    private MockMvc mockMvc;
+    @Mock
+    private ProductRepository productRepository;
 
+    @InjectMocks
+    private ProductService productService;
+
+    @Test
+    public void search_withNoFilters_shouldReturnAllProducts() {
+        Product featuredProduct = new Product(1, "Featured Product", 25.00, 2, "Newly featured product", "low-fat", 100, true, "image.url");
+        Product nonFeaturedProduct = new Product(1, "Non-Featured Product", 5.00, 1, "Newly featured product", "organic", 100, false, "image.url");
+
+        when(productRepository.findAll()).thenReturn(Arrays.asList(featuredProduct, nonFeaturedProduct));
+
+        List<Product> productList = productService.search(null,null,null,null,null);
+
+        assertEquals(2, productList.size());
+    }
 }
